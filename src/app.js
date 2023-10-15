4a5619a8c53bd5fd76ca9102bc9c611d2fe7b2be
Francisca Moore <fraanmoore@gmail.com>
2023-10-14 22:39:59 -0300

    comienzo del boton para ingresar las coordenadas

--- src/app.js
+++ src/app.js
@@ -6,23 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //hacer boton emergente para agregar las coordenadas
+  //agregar tablero de coordenadas
+  //que las coordenadas ingresadas las lea el tablero
 
-// 0 = empty
-// 1 = part of a ship
-// 2 = a sunken part of a ship
-// 3 = a missed shot
-let gameBoard = [
-  [1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
-];
+  // let fireTorpedo;
+  // function openFireTorpedo() {
+  //   fireTorpedo = onclick;
+  // }
+  // 0 = empty
+  // 1 = part of a ship
+  // 2 = a sunken part of a ship
+  // 3 = a missed shot
+  let gameBoard = [
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+  ];
+
+  function shoot(line, column) {
+    if (gameBoard[line][column] === 1) {
+      alert("Diste al blanco!");
+      gameBoard[line][column] = 2;
+    } else if (gameBoard[line][column] === 0) {
+      alert("Fallaste!");
+      gameBoard[line][column] = 3;
+    } else {
+      alert("Ya habías dispado aquí!");
+    }
+  }
+  //shoot(1, 1);
+};
